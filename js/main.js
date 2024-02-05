73fc156ac1c2293cc2b9272a89ae16d690d1152e
8kyu <guillemsenabre@gmail.com>
2024-02-05 14:46:26 +0100

    feat: extend project division with buttons (basic style) completed

--- js/main.js
+++ js/main.js
@@ -18,21 +18,32 @@ let count = 0;
 
 projectDiv.addEventListener('click', function (event) {
   if (event.target == projectDiv || event.target == projectList) {
+    
     // Get current height and create a variable to compute the new height
     let currentHeight = parseInt(window.getComputedStyle(projectDiv).height);
     let newHeight;
 
-    // Variable to store the new text alignment
+    // Store the new text alignment for Project division
     let newJustifyContent;
     let newPaddingTop;
 
+    // Store new display and visibility of projectList
+
+    let newDisplay;
+    let newVisibility;
+
     if (count%2 === 0) {
+
       // Double the height
       newHeight = currentHeight * 2;
 
       // from center-center to top-center
       newJustifyContent = 'flex-start'
       newPaddingTop = 5
+
+      // Visible project list (visually and phtsically)
+      newDisplay = 'grid'
+      newVisibility = 'visible'
 
 
     } else {
@@ -41,12 +52,17 @@ projectDiv.addEventListener('click', function (event) {
       newJustifyContent = 'center'
       newPaddingTop = 0
 
+      newDisplay = 'none'
+      newVisibility = 'hidden'
+
     }
 
     // Update the button's style
     projectDiv.style.justifyContent = `${newJustifyContent}`
     projectDiv.style.paddingTop = `${newPaddingTop}vh`
     projectDiv.style.height = `${newHeight}px`;
+    projectList.style.display = `${newDisplay}`
+    projectList.style.visibility = `${newVisibility}`
 
     // Controls the number of clicks
     count++;
